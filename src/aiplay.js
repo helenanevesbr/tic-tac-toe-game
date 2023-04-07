824a126f83d65dd39a6a82b5aed84b7393e5593a
Helena Neves <helenaneves.br@gmail.com>
2023-04-07 13:24:06 -0300

    AI finds empty squares before making a move

--- src/aiplay.js
+++ src/aiplay.js
@@ -1,11 +1,23 @@
+/* Now that my AI is connected to the game's state, it can know what is the current state of the board and make it's move.
+All I want for now is a basic move:  find empty squares and mark a random one.
+Not so smart as an AI can be, but we will roll with that for now.*/
+
 export default function aiPlay(history, state, setState) {
-    /*setState is a method provided by the React library, so why would I neeed to pass it down as argument?
-    Because I need to bind it to the correct context in order to update the state of the parent component
-    I'm passing a reference to the setState() method of the parent Game component, along with the correct context (this) to call it with.
-    This allows aiPlay() to call setState() and update the state of the parent Game component.
-    */
-   
-    console.log('history', history)
-    console.log('state',state)
-    console.log('setState',setState)
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    /*I was tempted to pass "squares" directly as an argument to aiPlay() instead of "history".
+    If all I wanted from state's property history was the last squares array, that would be all fine and good.
+    handleClick() would have already gone through the trouble of slicing history for aiPlay(), and I'd save some lines of coding (line 6 ad 7 specifically).
+    However, setState() relies on the history array to update the state of the Game component correctly.
+    When I call setState() in aiPlay() the same way I called it on handleClick(), you'll see what I mean*/
+
+    const emptySquares = squares.reduce((acc, square_value, i) =>
+        (square_value === null ? //The reduce function checks if the square_value is null, indicating an empty square.
+            acc.concat(i) //If it is, it concatenates the i value (the index of the empty square) to the acc array
+            : acc //If it isn't, the function simply returns the current value of acc.
+        ),
+        [] //the initial value of the accumulator
+    );
+
+    console.log(emptySquares)
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,10 +5,6 @@ import aiPlay from '../aiplay';
 
 /*This component manages the game state and renders the Board component*/
 
-/*I've added that last if statement to my handeClick.
-Now if an end user clicks on a square and that does not finishes the game, it will be my AI's turn to make a move
-But in order to make a move, it needs to "know" the current state of the Board: which squares are still empty?*/
-
 class Game extends React.Component {
 
   constructor(props) {
@@ -20,33 +16,12 @@ class Game extends React.Component {
       stepNumber: 0,
       xIsNext: true
     };
-    /*state is a property of the Game component class, not a standalone variable.
-    That is why whenever we update the game state, we need to refer to this property using this.state, instead of just "state"*/
-
   }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-
-    /*See? In order to get a copy of the current history and the last state of the squares, we need "this.state"
-    Mind you, this is what history looks like if our first move is X on square number 9, then O on square 1:
-
-    ~~ HISTORY ~~
-      history [{…}]
-        0: {squares: Array(9)} ---> squares: (9) [null, null, null, null, null, null, null, null, null]
-        length: 1[[Prototype]]: Array(0)
-      history (2) [{…}, {…}]
-        0: {squares: Array(9)} ---> squares: (9) [null, null, null, null, null, null, null, null, null]
-        1: {squares: Array(9)} ---> squares: (9) [null, null, null, null, null, null, null, null, 'X']
-        length: 2[[Prototype]]: Array(0)
-
-    What we are doing with local variable "current" is getting the last item in the history array:
-    - first click will set current = history[0], an array in which all squares are null
-    - second click will set current = history[1], an array in which the last square has value 'X'
-    Notice that, since handleClick() has not yet set a new state, the current state of the board is before the most recent click changes the board.
-    */
 
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -58,20 +33,12 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-
       stepNumber: history.length,
-
       xIsNext: !this.state.xIsNext,
     });
 
     if (!calculateWinner(squares) && !this.state.xIsNext) {
         aiPlay(history, this.state, this.setState);
-        /*history, stepNumber, and xIsNext are also properties of the Game component, just like state.
-        They are all stored as properties of the Game component's state object in the constructor.
-
-        So how come I need "this.state" to pass the state as an argument, but I don't need "this.state.history", just "history"?
-        It's because the argument for aiPlay() is not the history property stored in the state object
-        Rather, it's the local variable declared within the handleClick() method, line 29*/
     }
 
   }
